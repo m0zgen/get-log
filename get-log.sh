5686b15f957cd3285f4184ff69cfe62cb405220f
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-12-13 15:26:30 +0600

    + skip, collection dir, scp from remote to local

--- get-log.sh
+++ get-log.sh
@@ -20,7 +20,8 @@ usage() {
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -f|--file) _FILE=1; ;;
+        -s|--skip) _SKIP=1; ;;
+        -f|--file) _FILE=1 _FILE_DATA=$2; ;;
         -l|--list) _LIST=1 _LIST_DATA=$2; shift ;;
         -h|--help) usage ;; 
         *) _DEFAULT=1 ;;
@@ -33,25 +34,69 @@ done
 
 # Options
 if [[ "$_LIST" -eq "1" ]]; then
-    _SERVERS_LIST=` cat $_LIST_DATA`
+    _SERVERS_LIST=`cat $_LIST_DATA`
 else
     _SERVERS_LIST=`cat $SCRIPT_PATH/servers.txt`
+fi
+
+if [[ -z "$_FILE" ]]; then
+    echo "Set remote file path please. Exit."
+    exit 1
+else
+    _DEST_FILE=$_FILE_DATA  
 fi
 
 
 # Functions variables
 # ---------------------------------------------------\
+function getFile() {
+    echo "[✓] Get to: $1"
+
+    if ! ssh $2 "test -e $1" 2> /dev/null; then
+        echo "File $1 not exist"
+    else
+
+        local fileDir=$SCRIPT_PATH/collection
+        local serverDir=$fileDir/$2
+
+        if [[ ! -d "$fileDir" ]]; then
+            mkdir -p $fileDir
+        fi
+
+        if [[ ! -d "$serverDir" ]]; then
+            mkdir $serverDir
+        fi
+
+        echo "Copy $1 to $serverDir:"
+        scp -r -o IdentitiesOnly=yes $2:$1 $serverDir/
+
+
+    fi
+}
+
+function callFromTest() {
+
+    ping -c1 -W1 -q $_server &>/dev/null
+    status=$( echo $? )
+    if [[ $status == 0 ]] ; then
+         echo "[✓] Success: ${_server} available."
+         getFile $_FILE_DATA $_server
+    else
+         echo "[✗] Fail: ${_server} not available."
+    fi
+
+}
 
 function getServers() {
+
     for _server in ${_SERVERS_LIST}; do
-        
-        ping -c1 -W1 -q $_server &>/dev/null
-        status=$( echo $? )
-        if [[ $status == 0 ]] ; then
-             echo "[✓] Success: ${_server} available."
+
+        if [[ "$_SKIP" -eq "1" ]]; then
+            getFile $_FILE_DATA $_server
         else
-             echo "[✗] Fail: ${_server} not available."
+            callFromTest
         fi
+        
 
     done
 }
